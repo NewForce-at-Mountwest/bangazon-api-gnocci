0efaaea9a503379c72a5db2ece0fb322181a99bf
Tommy Spurlock <tspurloc@gmail.com>
2019-05-16 14:22:13 -0400

    commit to add models for tickets

---
+++ BangazonAPI/BangazonAPI/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public bool IsSuperViser { get; set; }
+
+        public Department CurrentDepartment { get; set; }
+
+    }
+}

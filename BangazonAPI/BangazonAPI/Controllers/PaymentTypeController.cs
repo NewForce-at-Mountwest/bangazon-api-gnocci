605c8b930b0969986898b636db6c0508efa0bccd
joeywellman <jppwellman@gmail.com>
2019-05-17 10:05:35 -0400

    merge with models

---
+++ BangazonAPI/BangazonAPI/Controllers/PaymentTypeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+//Ticket #3: Allow Developers to Access Payment Type Resource
+
+//# Feature Criteria:
+// - `GET`
+// - `POST`
+// - `PUT`
+// - `DELETE`
+// - User should be able to GET a list, and GET a single item.
+
+//## Testing Criteria
+// - Write a testing class and test methods that validate the GET single, GET all, POST, PUT, and DELETE operations work as expected.
+
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PaymentTypeController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

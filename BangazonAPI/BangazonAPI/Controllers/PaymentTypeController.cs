f5efa54905448176e6f1fffe1dcfe9256330ec80
joeywellman <jppwellman@gmail.com>
2019-05-17 10:18:50 -0400

    merge with model updates

--- BangazonAPI/BangazonAPI/Controllers/PaymentTypeController.cs
+++ BangazonAPI/BangazonAPI/Controllers/PaymentTypeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using BangazonAPI.Data;
+using BangazonAPI.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 
 //Ticket #3: Allow Developers to Access Payment Type Resource
@@ -23,36 +27,6 @@ namespace BangazonAPI.Controllers
     [Route("api/[controller]")]
     public class PaymentTypeController : Controller
     {
-        // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }

27e9f6a0596d8acb4a59b19d74f16f49790fa51f
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-05-20 10:50:55 -0400

    finished controller and testing worked

--- BangazonAPI/BangazonAPI/Controllers/CustomerController.cs
+++ BangazonAPI/BangazonAPI/Controllers/CustomerController.cs
@@ -30,8 +30,10 @@ namespace CustomerResource.Controllers
             }
         }
 
+
+        //Get method with two strings, 'include', 'q'
         [HttpGet]
-        public async Task<IActionResult> Get(string include)
+        public async Task<IActionResult> Get(string include, string q)
         {
             using (SqlConnection conn = Connection)
             {
@@ -46,7 +48,7 @@ namespace CustomerResource.Controllers
                     c.LastName AS 'Customer Last Name'";
                     string customerTable = "FROM Customer c";
 
-                    //Making String Query to show customer product type if they ask for it (?include=products)
+                    //Making Query string to show customer product type if they ask for it (?include=products)
                     if (include == "products")
                     {
                         string productColumn = @",p.Id AS 'Product Id',
@@ -55,6 +57,7 @@ namespace CustomerResource.Controllers
                                                   p.[Description] AS 'Product Description',
                                                   p.Quantity AS 'Product Quantity'";
                         string productTable = @"JOIN Product p ON c.Id = p.CustomerId";
+
                         //Making command = the query strings 
                         command = $@"{customerColumn}
                                      {productColumn}
@@ -62,12 +65,22 @@ namespace CustomerResource.Controllers
                                       {productTable}";
                     }
                     else
+                    // set command to = just customer information if the user does not add 'include'
                     {
                         command = $@"{customerColumn}
                                       {customerTable}";
                     }
+
+                    //If statement for 'q' string, user sets q='whatever' and find 'FirstName/LastName' that is LIKE what user sets 'q=';
+                    //Future reference, don't forget to add '%' outside {q}
+                    if (q != null)
+                    {
+                        command += $" WHERE c.FirstName LIKE '{q}%' OR c.LastName LIKE '{q}%'";
+                    }
+                    //Another query string, doing the same thing as product except w/ payments
                     if (include == "payments")
                     {
+
                         string paymentColumn = @",pm.Id AS 'Payment Id',
                                                   pm.Name AS 'Payment Name',
                                                   pm.AcctNumber AS 'Payment Account Number'";
@@ -77,16 +90,10 @@ namespace CustomerResource.Controllers
                                      {paymentColumn}
                                      {customerTable}
                                      {paymentTable}";
-
                     }
 
-                    //Making String Query to show customer payment type if they ask for it (?include=payments)
-                    //else
-                    //{
-                    //    //Else user does not include payments/product it will make command = just customer info
-                    //    command = $@"{customerTable}
-                    //                 {customerTable}";
-                    //}
+
+
 
 
                     cmd.CommandText = command;
@@ -96,12 +103,14 @@ namespace CustomerResource.Controllers
                     while (reader.Read())
                     {
                         Customer attempt = new Customer
+                        //Getting the information of the customer - FUTURE JOSH MAKE SURE YOU REFERENCE WHAT YOU SET THE COLUM NAME 'AS' --
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Customer Id")),
                             FirstName = reader.GetString(reader.GetOrdinal("Customer First Name")),
                             LastName = reader.GetString(reader.GetOrdinal("Customer Last Name"))
                         };
                         
+                        //Getting the information of the products if include == 'products'
                         if(include == "products")
                         {
                             Product currentProduct = new Product
@@ -113,19 +122,23 @@ namespace CustomerResource.Controllers
                                 Quantity = reader.GetInt32(reader.GetOrdinal("Product Quantity"))
                             };
 
+                            // Determining if customers list already has the current product in it
                             if(customers.Any(c => c.Id == attempt.Id))
                             {
+                                //Finds the product in the list (if it is in there)
                                 Customer thisCustomer = customers.Where(c => c.Id == attempt.Id).FirstOrDefault();
                                 thisCustomer.ProductList.Add(currentProduct);
                             }
                             else
                             {
+                                //if the product is not in the list it will add it
                                 attempt.ProductList.Add(currentProduct);
                                 customers.Add(attempt);
                             }
                         }
                         if (include == "payments")
                         {
+                            //Same thing as above but for payments
                             PaymentType currentPayment = new PaymentType
                             {
                                 Id = reader.GetInt32(reader.GetOrdinal("Payment Id")),
@@ -155,6 +168,8 @@ namespace CustomerResource.Controllers
                 }
             }
         }
+
+        //Get for single customer
         [HttpGet("{Id}", Name = "GetCustomer")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
@@ -186,6 +201,128 @@ namespace CustomerResource.Controllers
                     reader.Close();
 
                     return Ok(Customer);
+                }
+            }
+        }
+
+
+        //Post 'Add' Method
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Customer Customer)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Customer (FirstName, LastName)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@firstname, @lastname)";
+                    cmd.Parameters.Add(new SqlParameter("@firstname", Customer.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@lastname", Customer.LastName));
+                    int newId = (int)cmd.ExecuteScalar();
+                    Customer.Id = newId;
+                    return CreatedAtRoute("GetCustomer", new { id = newId }, Customer);
+                }
+            }
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Customer Customer)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Customer
+                                            SET FirstName=@first, 
+                                            LastName=@last
+                                            WHERE Id = @Id";
+                        cmd.Parameters.Add(new SqlParameter("@first", Customer.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@last", Customer.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@Id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CustomerExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        //added delete method for testing
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Customer WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CustomerExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
+
+        private bool CustomerExist(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            FirstName, LastName
+                        FROM Customer
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
                 }
             }
         }
